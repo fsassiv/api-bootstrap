9e351de866b7e134cf6171aef76473189b412825
Flavio Andrade <flavio.ssiv@gmail.com>
2025-11-14 14:18:15 -0300

    refactor(general): update

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -20,6 +20,8 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
+  app.setGlobalPrefix('api/v1');
+
   await app.listen(process.env.APIGATEWAY_PORT ?? 3100);
 }
 void bootstrap();
